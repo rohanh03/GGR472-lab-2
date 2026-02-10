a2931f2f15ae2ab0ef0530a7bd5d37bfb77c6e83
rohanh03 <rohan.hemrajani@mail.utoronto.ca>
2026-02-09 20:52:37 -0500

    Fixed map orientation and zoom, added mumbai landmark point data layer, created map container in css file

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm9oYW4yMDAzIiwiYSI6ImNtazRrYTVtMDA4OTEzbW91Y
 const map = new mapboxgl.Map({
     container: 'my-map',
     style: 'mapbox://styles/rohan2003/cmld7caru001a01ql6176cx3o',
-    center: [19.073, 72.928],
-    zoom: 9.58
+    center: [72.868, 19.075],
+    zoom: 10
 });
 
 map.on('load', () => {
+
     map.addSource('mumbai-lm-data', {
         type: 'vector',
         url: 'mapbox://rohan2003.cpbv0rmp'
-    })
+    });
+
+    map.addLayer({
+        'id': 'mumbai-lm-pt',
+        'type': 'cirlce',
+        'source': 'mumbai-lm-data',
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': '#007cbf'
+        }
+    });
 });
 
